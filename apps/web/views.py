f15368d04f33375bb3927e391ca2b8678e3a1185
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-03 20:05:43 +0100

    fix

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """
+    Главная страница основного сайта ATGilet.
+    Пока простая заглушка, позже сюда прикрутим нормальный layout.
+    """
+    return render(request, "web/index.html")
