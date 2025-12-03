78633943877d9cf92ceb4c479195e4036ec0ee4a
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-03 19:59:08 +0100

    Update settings.py

--- apps/atgilet/settings.py
+++ apps/atgilet/settings.py
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'atgilet.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "atgilet_es",
+        "USER": "atgilet_es",
+        "PASSWORD": "[FD`E2:Q<&.6|w+U",
+        "HOST": "localhost",   # или 127.0.0.1
+        "PORT": "3306",
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
 
