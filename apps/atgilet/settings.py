c64184a8946a99fc9f2e70d40a155c679c063d14
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-03 20:20:14 +0100

    Update settings.py

--- apps/atgilet/settings.py
+++ apps/atgilet/settings.py
@@ -129,8 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR.parent / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
