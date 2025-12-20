20837441ffe8999b1c6d0c88dd6d1458d6d8fe2d
MissoffDev <129719599+azohan79@users.noreply.github.com>
2025-12-20 17:06:38 +0100

    Update urls.py

--- apps/atgilet/urls.py
+++ apps/atgilet/urls.py
@@ -5,11 +5,11 @@ from .views import front_router
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # корень сайта — динамический роутер
-    path("", front_router, name="home"),
-
     # прямые адреса (если нужно заходить напрямую)
-    path("web/", include("web.urls")),
+    path("", include("web.urls")),
     path("maintenance/", include("maintenance.urls")),
     path("news/", include("news.urls", namespace="news")),
+    
+    # корень сайта — динамический роутер
+    path("", front_router, name="home"),
 ]
